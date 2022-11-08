e470d84507aaf863281814f3158b34798f060f7e
Prash0265 <kmrprashanth92@gmail.com>
2022-11-07 20:06:36 -0500

    First Commit

--- Prashanth_C0871509_FT/ViewController.swift
+++ Prashanth_C0871509_FT/ViewController.swift
@@ -26,6 +26,13 @@ class ViewController: UIViewController {
  
         
     }
+   // let infoAlert = UIAlertController(title: "Result", message: userInfo, preferredStyle: .alert)
+//    private func showError(_ alert: UIAlertController) {
+//        let action = UIAlertAction(title: "Result!", style: .cancel)
+//        alert.addAction(action)
+//        present(alert, animated: true)
+//    }
+    
     func isEven(_ n: Int) -> Bool {
         if n%2 == 0{
             return true
@@ -56,9 +63,17 @@ class ViewController: UIViewController {
       if isEven(currentNumber!){
            CorrectIncorrect.image = UIImage(named: "Correct")
           setRandomvalue()
+          let infoAlert = UIAlertController(title: "Result", message: "Correct answer", preferredStyle: .alert)
+          infoAlert.addAction(UIAlertAction(title: "Play again!", style: .default, handler: nil))
+          infoAlert.addAction(UIAlertAction(title: "Show Progress", style: .default, handler: nil))
+          self.present(infoAlert, animated: true)
       }else{
           CorrectIncorrect.image = UIImage(named: "InCorrect")
           setRandomvalue()
+          let infoAlert = UIAlertController(title: "Result", message: "Incorrect answer", preferredStyle: .alert)
+          infoAlert.addAction(UIAlertAction(title: "Play again!", style: .default, handler: nil))
+          infoAlert.addAction(UIAlertAction(title: "Show Progress", style: .default, handler: nil))
+          self.present(infoAlert, animated: true)
 
       }
 
@@ -69,9 +84,17 @@ class ViewController: UIViewController {
         if isEven(currentNumber!){
             CorrectIncorrect.image = UIImage(named: "InCorrect")
             setRandomvalue()
+            let infoAlert = UIAlertController(title: "Result", message: "Incorrect answer", preferredStyle: .alert)
+            infoAlert.addAction(UIAlertAction(title: "Play again!", style: .default, handler: nil))
+            infoAlert.addAction(UIAlertAction(title: "Show Progress", style: .default, handler: nil))
+            self.present(infoAlert, animated: true)
         }else{
             CorrectIncorrect.image = UIImage(named: "Correct")
             setRandomvalue()
+            let infoAlert = UIAlertController(title: "Result", message: "Correct answer", preferredStyle: .alert)
+            infoAlert.addAction(UIAlertAction(title: "Play again!", style: .default, handler: nil))
+            infoAlert.addAction(UIAlertAction(title: "Show Progress", style: .default, handler: nil))
+            self.present(infoAlert, animated: true)
         }
         
     }
